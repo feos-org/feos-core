00904100eb2beb66cf75869bcb04973c75f34e27
Gernot Bauer <gernotbauer85@googlemail.com>
2022-03-14 17:34:33 +0100

    Move python equation of states and modules to build_wheel

---
+++ build_wheel/src/user_defined.rs
@@ -0,0 +1,69 @@
+use feos_core::python::user_defined::*;
+use feos_core::python::statehd::*;
+use feos_core::*;
+use numpy::convert::ToPyArray;
+use numpy::{PyArray1, PyArray2};
+use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
+use quantity::python::*;
+use quantity::si::*;
+use std::collections::HashMap;
+use std::rc::Rc;
+
+
+/// Equation of state implemented as python class.
+///
+/// Parameters
+/// ----------
+/// obj : python class object
+///     The class that implements the equation of state.
+///
+/// Returns
+/// -------
+/// UserDefinedEos
+///
+/// Raises
+/// ------
+/// RunTimeError
+///     If the class does not implement all necessary methods.
+#[pyclass(name = "UserDefinedEos", unsendable)]
+#[derive(Clone)]
+#[pyo3(text_signature = "(obj)")]
+pub struct PyUserDefinedEos(Rc<PyEoSObj>);
+
+#[pymethods]
+impl PyUserDefinedEos {
+    #[new]
+    fn new(obj: Py<PyAny>) -> PyResult<Self> {
+        Ok(Self(Rc::new(PyEoSObj::new(obj)?)))
+    }
+}
+
+impl_equation_of_state!(PyUserDefinedEos);
+impl_virial_coefficients!(PyUserDefinedEos);
+impl_state!(PyEoSObj, PyUserDefinedEos);
+impl_state_molarweight!(PyEoSObj, PyUserDefinedEos);
+impl_vle_state!(PyEoSObj, PyUserDefinedEos);
+
+#[pymodule]
+pub fn user_defined(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyStateHD>()?;
+    m.add_class::<PyStateD>()?;
+    m.add_class::<PyStateDDV3>()?;
+    m.add_class::<PyStateD3>()?;
+    m.add_class::<PyStateF>()?;
+    m.add_class::<PyStateHDD>()?;
+    m.add_class::<PyStateHDDV2>()?;
+    m.add_class::<PyStateHDDV3>()?;
+    m.add_class::<PyStateD3D>()?;
+    m.add_class::<PyStateD3DV2>()?;
+    m.add_class::<PyStateD3DV3>()?;
+    m.add_class::<PyUserDefinedEos>()?;
+    m.add_class::<PyState>()?;
+    m.add_class::<PyPhaseEquilibrium>()?;
+    m.add_class::<PyPhaseDiagramPure>()?;
+    m.add_class::<PyPhaseDiagramBinary>()?;
+    m.add_class::<PyPhaseDiagramHetero>()?;
+    m.add_class::<PyPhaseEquilibrium>()?;
+    Ok(())
+}
--- src/python/mod.rs
+++ src/python/mod.rs
@@ -1,47 +1,19 @@
 use crate::EosError;
 use pyo3::exceptions::PyRuntimeError;
-use pyo3::prelude::*;
-use pyo3::{wrap_pymodule, PyErr};
-use quantity::python::__PYO3_PYMODULE_DEF_QUANTITY;
+use pyo3::PyErr;
 
-mod cubic;
+pub mod cubic;
 mod equation_of_state;
 pub mod joback;
 pub mod parameter;
 mod phase_equilibria;
 mod state;
-mod statehd;
-mod user_defined;
+pub mod statehd;
+pub mod user_defined;
 mod utils;
-
-pub use cubic::__PYO3_PYMODULE_DEF_CUBIC;
-pub use user_defined::__PYO3_PYMODULE_DEF_USER_DEFINED;
 
 impl From<EosError> for PyErr {
     fn from(e: EosError) -> PyErr {
         PyRuntimeError::new_err(e.to_string())
     }
-}
-
-#[pymodule]
-pub fn feos_core(py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pymodule!(quantity))?;
-    m.add_wrapped(wrap_pymodule!(user_defined))?;
-    m.add_wrapped(wrap_pymodule!(cubic))?;
-    py.run(
-        "\
-import sys
-quantity.SINumber.__module__ = 'feos_core.si'
-quantity.SIArray1.__module__ = 'feos_core.si'
-quantity.SIArray2.__module__ = 'feos_core.si'
-quantity.SIArray3.__module__ = 'feos_core.si'
-quantity.SIArray4.__module__ = 'feos_core.si'
-sys.modules['feos_core.si'] = quantity
-sys.modules['feos_core.user_defined'] = user_defined
-sys.modules['feos_core.cubic'] = cubic
-    ",
-        None,
-        Some(m.dict()),
-    )?;
-    Ok(())
 }
